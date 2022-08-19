b5ebb0800272c16e36c3cd60d82bf98a4663bb05
ThatGravyBoat <thatgravyboat@gmail.com>
2022-08-19 16:11:02 -0230

    Fixed things.

--- fabric/src/main/java/tech/thatgravyboat/goodall/common/registry/fabric/ModSpawnsImpl.java
+++ fabric/src/main/java/tech/thatgravyboat/goodall/common/registry/fabric/ModSpawnsImpl.java
@@ -1,6 +1,5 @@
 package tech.thatgravyboat.goodall.common.registry.fabric;
 
-import net.fabricmc.fabric.mixin.object.builder.SpawnRestrictionAccessor;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.SpawnPlacements;
@@ -8,6 +7,6 @@ import net.minecraft.world.level.levelgen.Heightmap;
 
 public class ModSpawnsImpl {
     public static <T extends Mob> void setSpawnRules(EntityType<T> entityType, SpawnPlacements.Type location, Heightmap.Types type, SpawnPlacements.SpawnPredicate<T> predicate) {
-        SpawnRestrictionAccessor.callRegister(entityType, location, type, predicate);
+        SpawnPlacements.register(entityType, location, type, predicate);
     }
 }
